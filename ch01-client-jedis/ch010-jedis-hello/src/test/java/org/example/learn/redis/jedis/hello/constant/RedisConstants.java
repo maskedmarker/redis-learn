fb733bec67add6247762c671dd258d665c3bfdfd
maskedmarker <changjianghui0822@gmail.com>
2024-09-11 20:05:09 +0800

    调整项目结构

--- ch01-client-jedis/ch010-jedis-hello/src/test/java/org/example/learn/redis/jedis/hello/constant/RedisConstants.java
+++ ch01-client-jedis/ch010-jedis-hello/src/test/java/org/example/learn/redis/jedis/hello/constant/RedisConstants.java
@@ -2,6 +2,7 @@ package org.example.learn.redis.jedis.hello.constant;
 
 public class RedisConstants {
 
-    public static final String HOST = "192.168.175.129";
+//    public static final String HOST = "192.168.175.129";
+    public static final String HOST = "127.0.0.1";
     public static final int PORT = 6379;
 }
